b19d5c216dc95351c602dc5324bdd612c4bc252f
zhangtao <ztao8@qq.com>
2017-06-30 12:34:10 +0800

    3''

--- project/Application/Api/Controller/IndexController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
-    	echo '1132423';
-    }
-}
--- project/Common/Conf/const.php
+++
@@ -1,7 +0,0 @@
-<?php
-// 当前网址
-define('SITE_NAME', '久爱网');
-define('SITE_URL', 'http://www.92ws.cn');
-define('SITE_MEMBER_URL', 'http://member.92ws.cn');
-define('SITE_MANAGE_URL', 'http://manage.92ws.cn');
-define('UPLOAG_MAGE_URL', 'http://img.92ws.cn/');
