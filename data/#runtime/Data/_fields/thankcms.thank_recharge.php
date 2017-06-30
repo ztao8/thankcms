2aed79becc6ec182201b071325b4e3f35507addf
zhangtao <ztao8@qq.com>
2017-06-30 14:33:51 +0800

    '秀爱'

--- data/#runtime/Data/_fields/thankcms.thank_recharge.php
+++
@@ -1,1 +0,0 @@
-a:8:{i:0;s:2:"id";i:1;s:9:"member_id";i:2;s:6:"amount";i:3;s:6:"remark";i:4;s:6:"status";i:5;s:11:"create_time";s:3:"_pk";s:2:"id";s:5:"_type";a:6:{s:2:"id";s:16:"int(11) unsigned";s:9:"member_id";s:16:"int(11) unsigned";s:6:"amount";s:13:"decimal(10,0)";s:6:"remark";s:12:"varchar(200)";s:6:"status";s:10:"tinyint(1)";s:11:"create_time";s:16:"int(11) unsigned";}}
--- data/#runtime/Data/_fields/thankcms.thank_role.php
+++
@@ -1,1 +0,0 @@
-a:7:{i:0;s:2:"id";i:1;s:5:"title";i:2;s:6:"remark";i:3;s:6:"status";i:4;s:5:"rules";s:3:"_pk";s:2:"id";s:5:"_type";a:5:{s:2:"id";s:21:"mediumint(8) unsigned";s:5:"title";s:12:"varchar(100)";s:6:"remark";s:12:"varchar(255)";s:6:"status";s:10:"tinyint(1)";s:5:"rules";s:12:"varchar(500)";}}
